e94065c7e9341e9ace47d77258c7a6a0786c18b2
Unknown <tothistvan314@gmail.com>
2018-04-08 14:25:29 +0200

    Katona_felkesz

---
+++ zh2011A234/src/sereg/Katona.java
@@ -0,0 +1,19 @@
+package sereg;
+
+public class Katona {
+
+	private int tamadoero;
+	private int vedoero;
+	
+	//default konstruktor
+	public Katona() {
+		tamadoero=5;
+		vedoero=5;
+	}
+	
+	
+	public Katona(int tamadoero,int vedoero){
+		this.tamadoero=tamadoero;
+		this.vedoero=vedoero;
+	}
+}
